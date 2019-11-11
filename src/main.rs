a3d55445107c055f610b16aae4445dd21c8f6c2a
Killian Kemps <developer@killiankemps.fr>
2019-11-11 23:09:04 +0100

    Add / minimal request to Gopher server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Welcome to rs-gopher-client");
+  let server = "khzae.net";
+  match TcpStream::connect(format!("{}:70", server)) {
+    Ok(mut stream) => {
+      println!("Connected to {}!", server);
+
+      let msg = b"/\r\n";
+
+      stream.write(msg).unwrap();
+      println!("Sent /, awaiting reply...");
+
+      //let mut buffer = String::new();
+      let mut buffer = Vec::new();
+
+      match stream.read_to_end(&mut buffer) {
+        Ok(_) => {
+          let text = from_utf8(&buffer).unwrap();
+          println!("response: {}", text);
+        },
+        Err(e) => {
+          println!("Failed to receive data: {}", e);
+        }
+      }
+    },
+
+    Err(e) => {
+      println!("Failed to connect: {}", e);
+    }
+  }
+  println!("Terminated.");
 }
