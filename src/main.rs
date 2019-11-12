b5af09097652495d44115fac42678dc932dcd4a1
Killian Kemps <developer@killiankemps.fr>
2019-11-12 22:40:59 +0100

    Allow user to enter command

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,55 @@ use std::io::{Read, Write, stdin};
 
 fn main() {
   println!("Welcome to rs-gopher-client");
-  println!("Please enter server:");
 
-  let mut server = String::new();
-  stdin().read_line(&mut server)
-    .expect("Failed to read line");
+  loop {
+    println!("Please enter command:");
 
-  server = server.trim().to_string();
+    let mut command = String::new();
+    stdin().read_line(&mut command)
+      .expect("Failed to read line");
 
-  println!("\nConnecting to {}...", server);
-  match TcpStream::connect(format!("{}:70", server)) {
-    Ok(mut stream) => {
-      println!("Connected!\n");
+    command = String::from(command.trim());
 
-      let msg = b"/\r\n";
-
-      stream.write(msg).unwrap();
-
-      let mut buffer = String::new();
-
-      match stream.read_to_string(&mut buffer) {
-        Ok(_) => {
-          println!("{}", &buffer);
-        },
-        Err(e) => {
-          println!("Failed to receive data: {}", e);
-        }
+    let mut server = String::new();
+    if command.contains("go") {
+      match command.get(3..) {
+        Some(contains) => server = contains.to_string(),
+        None => println!("Does not contain go"),
       }
-    },
+    }
+    else if command.contains("quit") {
+      println!("Terminated.");
+        break;
+    }
+    else {
+      continue;
+    }
 
-    Err(e) => {
-      println!("Failed to connect: {}", e);
+    println!("\nConnecting to {}...", server);
+    match TcpStream::connect(format!("{}:70", server)) {
+      Ok(mut stream) => {
+        println!("Connected!\n");
+
+        let msg = b"/\r\n";
+
+        stream.write(msg).unwrap();
+
+        let mut buffer = String::new();
+
+        match stream.read_to_string(&mut buffer) {
+          Ok(_) => {
+            println!("{}", &buffer);
+          },
+          Err(e) => {
+            println!("Failed to receive data: {}", e);
+          }
+        }
+      },
+
+      Err(e) => {
+        println!("Failed to connect: {}", e);
+      }
     }
   }
-  println!("Terminated.");
 }
