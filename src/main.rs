8e090c53e09d1d53d3a2a0bfcfa77c7a83f0b124
Killian Kemps <killian.kemps@lrqdo.fr>
2019-11-14 15:58:38 +0100

    Allow user to enter full URL

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
 use std::net::{TcpStream};
 use std::io::{Read, Write, stdin};
+use regex::Regex;
+
+struct GopherURL {
+  host: String,
+  port: String,
+  path: String,
+}
+
+impl GopherURL {
+  fn new() -> GopherURL {
+    GopherURL {
+      host: "".to_string(),
+      port: "".to_string(),
+      path: "".to_string(),
+    }
+  }
+
+  fn from(url: &str) -> GopherURL {
+    let re = Regex::new(r"(gopher://)?([a-zA-Z0-9.]+):?([0-9]+)?([a-zA-Z0-9/.]+)?").unwrap();
+    let captures = re.captures(url).unwrap();
+
+    GopherURL {
+      host: captures.get(2).map_or("", |m| m.as_str()).to_string(),
+      port: captures.get(3).map_or("70", |m| m.as_str()).to_string(),
+      path: captures.get(4).map_or("/", |m| m.as_str()).to_string(),
+    }
+  }
+
+  fn get_server(&self) -> String {
+    return format!("{}:{}", &self.host, &self.port);
+  }
+}
 
 fn main() {
   println!("Welcome to rs-gopher-client");
@@ -13,10 +45,12 @@ fn main() {
 
     command = String::from(command.trim());
 
-    let mut server = String::new();
+    let mut url = GopherURL::new();
     if command.contains("go") {
       match command.get(3..) {
-        Some(contains) => server = contains.to_string(),
+        Some(contains) => {
+          url = GopherURL::from(&contains);
+        },
         None => println!("Does not contain go"),
       }
     }
@@ -28,14 +62,12 @@ fn main() {
       continue;
     }
 
-    println!("\nConnecting to {}...", server);
-    match TcpStream::connect(format!("{}:70", server)) {
+    println!("\nConnecting to {}...", url.get_server());
+    match TcpStream::connect(url.get_server()) {
       Ok(mut stream) => {
         println!("Connected!\n");
 
-        let msg = b"/\r\n";
-
-        stream.write(msg).unwrap();
+        stream.write(format!("{}\r\n", url.path).as_bytes()).unwrap();
 
         let mut buffer = String::new();
 
