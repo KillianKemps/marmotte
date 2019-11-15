c68729418579992f29e4937f0530ae91ccdefed2
Killian Kemps <killian.kemps@lrqdo.fr>
2019-11-15 16:02:18 +0100

    Parse Gopher menu and default responses

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,139 @@ use regex::Regex;
 struct GopherURL {
   host: String,
   port: String,
-  path: String,
+  r#type: String,
+  selector: String,
 }
 
 impl GopherURL {
   fn new() -> GopherURL {
     GopherURL {
-      host: "".to_string(),
-      port: "".to_string(),
-      path: "".to_string(),
+      host: String::new(),
+      port: String::new(),
+      r#type: String::new(),
+      selector: String::new(),
     }
   }
 
   fn from(url: &str) -> GopherURL {
-    let re = Regex::new(r"(gopher://)?([a-zA-Z0-9.]+):?([0-9]+)?([a-zA-Z0-9/.]+)?").unwrap();
+    let re = Regex::new(r"(gopher://)?([a-zA-Z0-9.]+):?([0-9]+)?(/[a-z0-9]+)?([a-zA-Z0-9/.~\-_]+)?").unwrap();
     let captures = re.captures(url).unwrap();
 
     GopherURL {
       host: captures.get(2).map_or("", |m| m.as_str()).to_string(),
       port: captures.get(3).map_or("70", |m| m.as_str()).to_string(),
-      path: captures.get(4).map_or("/", |m| m.as_str()).to_string(),
+      r#type: captures.get(4).map_or("/1", |m| m.as_str())[1..].to_string(),
+      selector: captures.get(5).map_or("/", |m| m.as_str()).to_string(),
     }
   }
 
   fn get_server(&self) -> String {
     return format!("{}:{}", &self.host, &self.port);
+  }
+}
+
+struct GopherMenuLine {
+  r#type: String,
+  description: String,
+  selector: String,
+  host: String,
+  port: String,
+}
+
+impl GopherMenuLine {
+  fn new() -> GopherMenuLine {
+    GopherMenuLine {
+      r#type: String::new(),
+      description: String::new(),
+      selector: String::new(),
+      host: String::new(),
+      port: String::new(),
+    }
+  }
+
+  fn from(line: &str) -> GopherMenuLine {
+    let re = Regex::new(r"^([a-z0-9])([^\t]*)?\t([^\t]*)?\t([^\t]*)?\t([0-9]*)?").unwrap();
+    let captures = re.captures(&line).unwrap();
+
+    GopherMenuLine {
+      r#type: captures.get(1).map_or("i", |m| m.as_str()).to_string(),
+      description: captures.get(2).map_or("", |m| m.as_str()).to_string(),
+      selector: captures.get(3).map_or("", |m| m.as_str()).to_string(),
+      host: captures.get(4).map_or("", |m| m.as_str()).to_string(),
+      port: captures.get(5).map_or("", |m| m.as_str()).to_string(),
+    }
+  }
+}
+
+struct GopherMenuResponse {
+  lines: Vec<GopherMenuLine>,
+}
+
+impl GopherMenuResponse {
+  fn new() -> GopherMenuResponse {
+    GopherMenuResponse {
+      lines: Vec::new(),
+    }
+  }
+
+  fn from(response: &str) -> GopherMenuResponse {
+    let mut lines = Vec::new();
+    let mut gopherline = GopherMenuLine::new();
+
+    for line in response.split("\n").collect::<Vec<&str>>() {
+      // dot indicates end of response
+      if line.starts_with(".") || line == "" {
+        break;
+      }
+
+      gopherline = GopherMenuLine::from(line);
+      lines.push(gopherline);
+    }
+
+    GopherMenuResponse {
+      lines,
+    }
+  }
+
+  fn display(&self) {
+    for line in &self.lines {
+      println!("{}{}\t{}\t{}\t{}", line.r#type, line.description, line.selector, line.host, line.port);
+    }
+  }
+}
+
+struct GopherDefaultResponse {
+  lines: Vec<String>,
+}
+
+impl GopherDefaultResponse {
+  fn new() -> GopherDefaultResponse {
+    GopherDefaultResponse {
+      lines: Vec::new(),
+    }
+  }
+
+  fn from(response: &str) -> GopherDefaultResponse {
+    let mut lines = Vec::new();
+
+    for line in response.split("\n").collect::<Vec<&str>>() {
+      // dot indicates end of response
+      if line.starts_with(".") {
+        break;
+      }
+
+      lines.push(line.to_string());
+    }
+
+    GopherDefaultResponse {
+      lines,
+    }
+  }
+
+  fn display(&self) {
+    for line in &self.lines {
+      println!("{}", line);
+    }
   }
 }
 
@@ -56,7 +164,7 @@ fn main() {
     }
     else if command.contains("quit") {
       println!("Terminated.");
-        break;
+      break;
     }
     else {
       println!("Please enter one of following command:\n\
@@ -70,13 +178,21 @@ fn main() {
       Ok(mut stream) => {
         println!("Connected!\n");
 
-        stream.write(format!("{}\r\n", url.path).as_bytes()).unwrap();
+        stream.write(format!("{}\r\n", url.selector).as_bytes()).unwrap();
 
         let mut buffer = String::new();
 
         match stream.read_to_string(&mut buffer) {
           Ok(_) => {
-            println!("{}", &buffer);
+            // Parse Gopher menu according to Gopher selector
+            if url.r#type == "1" {
+              let response = GopherMenuResponse::from(&buffer);
+              response.display();
+            }
+            else {
+              let response = GopherDefaultResponse::from(&buffer);
+              response.display();
+            }
           },
           Err(e) => {
             println!("Failed to receive data: {}", e);
